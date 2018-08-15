d91c98dd714ce80ffbb97bb204d469be028611f4
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-15 20:56:41 +0000

    Done.

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function theBeatlesPlay(musicians, instruments) {
   var newArray = []
   
   for (var index = 0; index > musicians.arrayLength - 1; index++) {
-    newArray[index] = `'${musicians[index]} plays ${instruments[index]}'`
-    return newArray
+    newArray[index] = `${musicians[index]} plays ${instruments[index]}`
   }
+    return newArray
 }
 /*
 The function should also contain a for loop which loops over the array of musicians. You'll want to be careful about what value you set your counter variable to store. (Hint: Think about what the first index of an array is). The first time through the loop, the body of the loop should create a string using the first index of the musicians array and the first index of the instruments array: "John Lennon plays guitar". This string should be added to the empty array you created. The loop should make the same sentence for every member of the musicians array. The function should return the array of new strings.
